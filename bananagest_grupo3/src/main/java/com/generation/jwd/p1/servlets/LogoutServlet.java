073bb497f898ecfe4b31b866517f26aee278074d
mdguay123 <danielm_95@hotmail.com>
2018-01-19 15:47:35 +0100

    .

--- bananagest_grupo3/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
+++ bananagest_grupo3/src/main/java/com/generation/jwd/p1/servlets/LogoutServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-   
+   //doing
     public LogoutServlet() {
         super();
     }
